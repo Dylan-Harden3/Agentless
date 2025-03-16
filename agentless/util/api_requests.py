dce67bd746e22c29b65baae50672e8e33c994bb9
Dylan Harden <dylanharden33@gmail.com>
2025-03-15 21:04:01 -0500

    Refactor model to use gemini api

--- agentless/util/api_requests.py
+++ agentless/util/api_requests.py
@@ -1,10 +1,10 @@
 import time
 from typing import Dict, Union
 
-import anthropic
 import openai
 import tiktoken
-
+from dotenv import load_dotenv
+import os
 
 def num_tokens_from_messages(message, model="gpt-3.5-turbo-0301"):
     """Returns the number of tokens used by a list of messages."""
@@ -55,11 +55,12 @@ def handler(signum, frame):
     raise Exception("end of time")
 
 
-def request_chatgpt_engine(config, logger, base_url=None, max_retries=40, timeout=100):
+def request_chatgpt_engine(config, logger, base_url="https://generativelanguage.googleapis.com/v1beta/openai/", max_retries=40, timeout=100):
     ret = None
     retries = 0
 
-    client = openai.OpenAI(base_url=base_url)
+    load_dotenv()
+    client = openai.OpenAI(base_url=base_url, api_key=os.getenv("API_KEY"))
 
     while ret is None and retries < max_retries:
         try:
@@ -96,69 +97,4 @@ def request_chatgpt_engine(config, logger, base_url=None, max_retries=40, timeou
         retries += 1
 
     logger.info(f"API response {ret}")
-    return ret
-
-
-def create_anthropic_config(
-    message: str,
-    max_tokens: int,
-    temperature: float = 1,
-    batch_size: int = 1,
-    system_message: str = "You are a helpful assistant.",
-    model: str = "claude-2.1",
-    tools: list = None,
-) -> Dict:
-    if isinstance(message, list):
-        config = {
-            "model": model,
-            "temperature": temperature,
-            "max_tokens": max_tokens,
-            "messages": message,
-        }
-    else:
-        config = {
-            "model": model,
-            "temperature": temperature,
-            "max_tokens": max_tokens,
-            "messages": [
-                {"role": "user", "content": [{"type": "text", "text": message}]},
-            ],
-        }
-
-    if tools:
-        config["tools"] = tools
-
-    return config
-
-
-def request_anthropic_engine(
-    config, logger, max_retries=40, timeout=500, prompt_cache=False
-):
-    ret = None
-    retries = 0
-
-    client = anthropic.Anthropic()
-
-    while ret is None and retries < max_retries:
-        try:
-            start_time = time.time()
-            if prompt_cache:
-                # following best practice to cache mainly the reused content at the beginning
-                # this includes any tools, system messages (which is already handled since we try to cache the first message)
-                config["messages"][0]["content"][0]["cache_control"] = {
-                    "type": "ephemeral"
-                }
-                ret = client.beta.prompt_caching.messages.create(**config)
-            else:
-                ret = client.messages.create(**config)
-        except Exception as e:
-            logger.error("Unknown error. Waiting...", exc_info=True)
-            # Check if the timeout has been exceeded
-            if time.time() - start_time >= timeout:
-                logger.warning("Request timed out. Retrying...")
-            else:
-                logger.warning("Retrying after an unknown error...")
-            time.sleep(10 * retries)
-        retries += 1
-
     return ret
